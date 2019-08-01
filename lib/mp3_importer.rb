06951df8e2cb1bd2ebf849cf83bd3a9aea1ff0c8
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-01 03:55:15 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,15 @@
 require 'pry'
 class MP3Importer
 
-  attr_accessor
+@directory = []
 
   def initialize(file_path)
-    file_path = @file_path
 
     #binding.pry
   end
 
+def files
+  file_path_parse = file_path.split("/")
+  file_path_directory = file_path_parse[2]
+  @directory << file_path_directory
 end
