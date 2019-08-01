9d1ee30fc6d8ff7f3b51f2ab81c921508d7a4c06
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-01 04:53:38 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,17 +3,18 @@ class MP3Importer
 
 attr_accessor :path
 
-
+list_of_filenames = []
 
   def initialize(path)
     @path = path
-      @files = []
+
     #binding.pry
   end
 
 
     def files
       Dir.entries(path).select do | file | !File.directory? file
+        list_of_filenames << file
       #  binding.pry
     end
 
