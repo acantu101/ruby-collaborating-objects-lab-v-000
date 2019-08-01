698b6ff8b40df5a67dac5aae106881068e920337
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-01 02:48:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,8 @@ class Song
 
   def self.new_by_filename(filename)
     song = self.new(filename)
-    song.artist = filename.split(" - ")[0]
-    song.artist.name = filename.split(" - ")[1]
+    song.artist.name = filename.split(" - ")[0]
+    song.name = filename.split(" - ")[1]
     song
 
 
